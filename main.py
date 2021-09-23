af92ad5d300f2383afd421df89146a2bc1124402
Flávio Quirino <82730592+f-quirino@users.noreply.github.com>
2021-09-23 16:32:39 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-pass
+import pandas as pd
+
+df_conexoes = pd.read_csv('conexoes.csv', encoding = 'utf-8')
+df_usuarios = pd.read_csv('usuarios.csv', encoding = 'utf-8')
+
+df_conexoes.head()
+class Grafo():
+    def __init__(self):
+        self.adjacencia = {}
+
+    def adiciona(self, vertice):
+        # Para adicionar um vertice, simplesmente criamos a chave dele dentro nosso dicionario de adjacencia
+        self.adjacencia[vertice] = {}
+    
+    def conecta(self, origem, destino, peso = 1):
+        # Acessamos nosso vertice e criamos uma chave para a conexao dele, atribuindo o valor como sendo o peso
+        self.adjacencia[origem][destino] = peso
+        self.adjacencia[destino][origem] = peso
+
+
