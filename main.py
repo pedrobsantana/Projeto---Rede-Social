d8fb62a0bb222957867cf627a6d4bfe0e658d007
Flávio Quirino <82730592+f-quirino@users.noreply.github.com>
2021-09-24 15:29:55 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,16 +1,11 @@
 import csv
 
-#caminho_conexoes = 'C:\\Users\\Flávio Quirino\\Desktop\\Programação\\Curso Lets Code - Pi DS\\Projetos\\Estruturas de Dados\\Projeto-Rede-Social\\conexoes.csv'
-#caminho_usuarios = 'C:\\Users\\Flávio Quirino\\Desktop\\Programação\\Curso Lets Code - Pi DS\\Projetos\\Estruturas de Dados\\Projeto-Rede-Social\\usuarios.csv'
+caminho_conexoes = 'C:\\Users\\Flávio Quirino\\Desktop\\Programação\\Curso Lets Code - Pi DS\\Projetos\\Estruturas de Dados\\Projeto-Rede-Social\\conexoes.csv'
+caminho_usuarios = 'C:\\Users\\Flávio Quirino\\Desktop\\Programação\\Curso Lets Code - Pi DS\\Projetos\\Estruturas de Dados\\Projeto-Rede-Social\\usuarios.csv'
 
-#df_conexoes = open(caminho_conexoes, 'r', encoding = 'utf-8')
-df_conexoes = open("conexoes.csv", 'r', encoding = 'utf-8')
-reader_conexoes = csv.reader(df_conexoes)
 
-#df_usuarios = open(caminho_usuarios, 'r', encoding = 'utf-8')
-df_usuarios = open("usuarios.csv", 'r', encoding = 'utf-8')
-reader_usuarios = csv.reader(df_usuarios)
-
+reader_conexoes = csv.reader(open(caminho_conexoes, 'r', encoding = 'utf-8'))
+reader_usuarios = csv.reader(open(caminho_usuarios, 'r', encoding = 'utf-8'))
 class Grafo():
     def __init__(self):
         self.adjacencia = {}
@@ -22,6 +17,82 @@ class Grafo():
     def conecta(self, origem, destino, peso = 1):
         # Acessamos nosso vertice e criamos uma chave para a conexao dele, atribuindo o valor como sendo o peso
         self.adjacencia[origem][destino] = peso
+
+    def exibir_perfil(self, nome):
+    
+        # seguindo
+        seguindo = 0
+        for el in self.adjacencia[nome].keys():
+            seguindo += 1
+        print(f"O usuário {nome} segue {seguindo} usuários.\n")
+
+        # seguidores
+        seguidores = []
+        for el in self.adjacencia.items():
+            for el1 in el[1].items():
+                if nome in el1:
+                    seguidores.append(el1) 
+
+        print(f"O usuário {nome} tem {len(seguidores)} seguidores.\n")
+
+
+    def story(self, nome):
+        
+        lista_amigos = list(self.adjacencia[nome].items())
+
+        melhores_amigos = []
+        amigos_comuns = []
+
+        # Separando 'Amigos comuns' dos 'Melhores Amigos'
+        for el in lista_amigos:
+            if el[1] == '2':
+                melhores_amigos.append(el[0])
+            else:
+                amigos_comuns.append(el[0])
+
+        # Bubblesort para melhores amigos
+        for i in range(len(melhores_amigos)):
+            for j in range(len(melhores_amigos)):
+                if melhores_amigos[j] > melhores_amigos[i]:
+                    melhores_amigos[i], melhores_amigos[j] = melhores_amigos[j], melhores_amigos[i]
+
+        #Bubblesort para amigos comuns
+        for i in range(len(amigos_comuns)):
+            for j in range(len(amigos_comuns)):
+                if amigos_comuns[j] > amigos_comuns[i]:
+                    amigos_comuns[i], amigos_comuns[j] = amigos_comuns[j], amigos_comuns[i]
+
+        #juntar ambas listas
+        lista_story = melhores_amigos + amigos_comuns
+        
+        print(f'Exibição de stories do usuario {nome}: {lista_story}')
+
+    def top_influencers(self, k):
+    
+        num_seguidores = {}
+
+        for usuario in self.adjacencia:
+            count = 0
+            for seguindo in self.adjacencia.items():
+                for el in seguindo[1]:
+                    if usuario in el:
+                        count += 1
+
+            num_seguidores[usuario] = count
+
+        # print(num_seguidores)
+
+        influencers = list(num_seguidores.items())
+
+        # Bubblesort para ordernar top influencers
+        for i in range(len(influencers)):
+            for j in range(len(influencers)):
+                if influencers[j][1] > influencers[i][1]:
+                    influencers[i], influencers[j] = influencers[j], influencers[i]
+
+        print(f'Tops {k} influencers\n')
+        for el in influencers[::-1][:k]:
+            print(f'{el[0]}: {el[1]}')
 
 def cria_grafo():
     g = Grafo()
@@ -36,23 +107,4 @@ def cria_grafo():
 g = cria_grafo()
 
 
-def exibir_perfil(nome):
-    # seguindo ****** verificar se precisa fazer validação de entrada (sting)
-    seguindo = 0
-    for el in g.adjacencia[nome].keys():
-        seguindo += 1
-
-    print(f"O usuário(a) {nome} segue {seguindo} usuários.\n")
-
-    # seguidores
-    seguidores = 0
-    for el in g.adjacencia.keys():
-        if nome == g.adjacencia[el].keys():
-            seguindo += 1
-
-    print(f"O usuário(a) {nome} tem {seguidores} seguidores.\n")
-
-
-exibir_perfil('helena42')
-
-print(g.adjacencia['helena42'].items())
+g.top_influencers(5)
