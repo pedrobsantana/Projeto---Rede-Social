47b1198b52417b8a7556292b976befa7f3cb9fbe
Flávio Quirino <flavioviniquirino@hotmail.com>
2021-09-27 20:42:07 -0300

    Ajuste de texto

--- main.py
+++ main.py
@@ -16,7 +16,6 @@ class Grafo():
         '''
         self.adjacencia[vertice] = {}
     
-
         
     def conecta(self, origem, destino, peso = 1):
         '''
@@ -28,7 +27,6 @@ class Grafo():
         '''
         self.adjacencia[origem][destino] = peso
 
-
         
     def exibir_perfil(self, nome):
         '''
@@ -36,12 +34,12 @@ class Grafo():
 
         Parametro: nome(username).
         '''
-        # seguindo
+        # Seguindo
         seguindo = 0
         for elemento in self.adjacencia[nome].keys():
             seguindo += 1
         
-        # seguidores
+        # Seguidores
         seguidores = []
         for elemento in self.adjacencia.items():
             for el in elemento[1].items():
@@ -50,12 +48,10 @@ class Grafo():
 
         print(f"O usuário {nome} tem {len(seguidores)} seguidores e segue {seguindo} usuários.\n")
 
-
-        
+      
     def stories(self, nome):
         '''
-        Definição: Função para ordenar a lista de Stories, 
-        por ordem alfabética com melhores amigos primeiro e depois amigos comuns.
+        Definição: Função para ordenar a lista de Stories, por ordem alfabética com melhores amigos primeiro e depois amigos comuns.
 
         Parametro: nome(username).
         '''
@@ -77,19 +73,18 @@ class Grafo():
                 if melhores_amigos[j] > melhores_amigos[i]:
                     melhores_amigos[i], melhores_amigos[j] = melhores_amigos[j], melhores_amigos[i]
 
-        #Bubblesort para Amigos Comuns
+        # Bubblesort para Amigos Comuns
         for i in range(len(amigos_comuns)):
             for j in range(len(amigos_comuns)):
                 if amigos_comuns[j] > amigos_comuns[i]:
                     amigos_comuns[i], amigos_comuns[j] = amigos_comuns[j], amigos_comuns[i]
 
-        #juntar as listas
+        # Juntar as listas
         lista_stories = melhores_amigos + amigos_comuns
         
         print(f'Exibição de stories do usuário {nome}: {lista_stories}\n')
+   
 
-
-        
     def top_influencers(self, k):
         '''
         Definição: Função para encontrar quem tem mais seguidores na rede, os Top Influencers.
@@ -118,27 +113,26 @@ class Grafo():
         print(f'*** TOP {k} INFLUENCERS ***')
         for elemento in influencers[::-1][:k]:
             print(f'{elemento[0]}: {elemento[1]}')
+    
 
-
-        
     def busca_caminho_usuarios(self, nome1, nome2):
         '''
         Definição: Função para encontrar o caminho entre uma pessoa e outra na rede.
 
-        Parametro: nome1(usuario primario), nome2(usuario destino)
+        Parametro: nome1(usuario primario), nome2(usuario)
         '''
         fila = [nome1]
         visitados = []
         predecessor = {nome1: None}
         
-        #enquanto tiver elementos na fila
+        # Enquanto tiver elementos na fila
         while len(fila) > 0:
             primeiro_elemento = fila[0]
             fila = fila[1:]
             visitados.append(primeiro_elemento)
             for adjacente in self.adjacencia[primeiro_elemento].keys():
                 
-                # se achou, monta o caminho
+                # Se achou, monta o caminho
                 if adjacente == nome2:
                     pred = primeiro_elemento
                     caminho_invertido = [nome2]
@@ -155,7 +149,8 @@ class Grafo():
                     predecessor[adjacente] = primeiro_elemento
                     fila.append(adjacente)
         return print(f"Não foi possivel encontrar um caminho entre {nome1} e {nome2}.")
-                        
+
+
 def cria_grafo():
     g = Grafo()
     for linha in reader_usuarios:
